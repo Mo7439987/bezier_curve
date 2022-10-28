242bbfca9c09a4289849fa9618e314569ce25451
Mo7439987 <90481633+Mo7439987@users.noreply.github.com>
2022-10-28 15:49:22 +0200

    c'est réparé

--- bezier_function.py
+++ bezier_function.py
@@ -2,7 +2,7 @@ from math import factorial
 import cv2
 import numpy as np
 from random import random, randint
-from math import sin, cos
+from math import sin, cos, sqrt
 from colorsys import hsv_to_rgb
 
 
@@ -40,9 +40,15 @@ def bezier(t, _points):
     return result
 
 
-def draw_bezier(_img, _points, step=(2 ** -8), radius=1, color=(255, 255, 255)):
+def draw_bezier(_img, _points: list, step=(2 ** -8), radius=1, color=(255, 255, 255), max_points=16):
     t = 0
     h, w, c = _img.shape
+
+    n = len(_points)
+    while n > max_points >= 3:
+        n = len(_points)
+        #print(n, end='')
+        _points.pop(0)
 
     while t <= 1:
         v = bezier(t, _points)
@@ -51,26 +57,37 @@ def draw_bezier(_img, _points, step=(2 ** -8), radius=1, color=(255, 255, 255)):
             y = int(v[1])
             c = float_to_int(hsv_to_rgb(t, 0.8, 0.8))
             cv2.circle(_img, (x, y), radius, c, thickness=-1)
-        t += step
+            #_img[y, x] = c
+        t += (step / sqrt(n))
 
 
 points = []
+print(id(points))
+iterations = 0
 
 
 def click_event(event, x, y, flags, params):
-    if event == cv2.EVENT_LBUTTONDOWN:
-        print(f'click {(x, y)}')
+    if event == cv2.EVENT_LBUTTONDOWN or True:
+        global iterations
+        iterations += 1
+        print(f'click {(x, y, len(points)), iterations}')
+        #print(id(points))
         points.append((x, y))
         shape = (1024, 1024, 3)
         img = np.zeros(shape, dtype=np.uint8)
-        draw_bezier(img, points, step=(2**-12), radius=8)
+        draw_bezier(img, points, step=(2**-3), radius=8, max_points=32)
 
         cv2.imshow('aj', img)
-        cv2.setMouseCallback('aj', click_event)
-        cv2.waitKey(0)
+        #cv2.setMouseCallback('aj', click_event)
+        #cv2.waitKey(0)
+    else:
+        print(event)
 
 
 if __name__ == '__main__':
+    l = [(random() * 1024, random() * 1024) for _ in range(1024)]
+    for _ in range(1023):
+        l.pop(0)
     shape = (1024, 1024, 3)
     img = np.zeros(shape, dtype=np.uint8)
     cv2.imshow('aj', img)
